7212bb2c55b5cdfa6b922918bc4045ec0e28357c
dnzambuli <damunzandm@outlook.com>
2024-03-10 15:33:36 +0300

    trending html

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import Banner from "./components/Banner";
 import Navbar from "./components/Navbar";
+import Trending from "./components/Trending";
 
 function App() {
   return (
     <>
       <Navbar />
       <Banner />
+      <Trending />
     </>
   );
 }
