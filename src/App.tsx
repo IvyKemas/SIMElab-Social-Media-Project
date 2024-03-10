13036fb6e11e7bef5d73be46309f2521c9edf657
dnzambuli <damunzandm@outlook.com>
2024-03-10 22:34:06 +0300

    the model can scrape ghafla and pulse

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import Banner from "./components/Banner";
+import Music from "./components/Music";
 import Navbar from "./components/Navbar";
 import Trending from "./components/Trending";
 
@@ -9,6 +10,7 @@ function App() {
       <Navbar />
       <Banner />
       <Trending />
+      <Music />
     </>
   );
 }
